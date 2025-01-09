57f24729e651328386f9647da5b614e4f4386485
mymoon_2004 <100510610+Dominic1324@users.noreply.github.com>
2025-01-09 18:27:43 +0900

    [Silver IV] Title: 큐, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,30 @@
+import sys
+li = []
+n = int(sys.stdin.readline())
+for i in range(n):
+    m = sys.stdin.readline()
+    if m[:2] == "pu":
+        m, num = m.split()
+        li += [int(num)]
+    elif m[0] == "p":
+        if len(li) == 0:
+            print(-1)
+        else:
+            print(li.pop(0))
+    elif m[0] == "s":
+        print(len(li))
+    elif m[0] == "e":
+        if len(li) == 0:
+            print(1)
+        else:
+            print(0)
+    elif m[0] == "f":
+        if len(li) == 0:
+            print(-1)
+        else:
+            print(li[0])
+    else:
+        if len(li) == 0:
+            print(-1)
+        else:
+            print(li[-1])
